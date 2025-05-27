c83aa9d1fa43c560742b999357836617a47fa06c
ramong26 <rlatndus21049@gmail.com>
2025-05-27 23:14:55 +0900

    refactor:전체 틀

--- src/features/product/components/ProductIdReview.tsx
+++ src/features/product/components/ProductIdReview.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import { useParams } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
@@ -32,24 +31,27 @@ export default function ProductIdReview() {
   if (isError) return <div>에러 발생: {(error as Error).message}</div>
 
   return (
-    <div className="flex items-center justify-between gap-10 text-[#f1f1f5]">
+    <div className="flex items-start justify-between gap-10 text-[#f1f1f5]">
       <img src={productData.image} width={306} height={228} alt="상품 이미지" />
+
       <div className="w-[545px] ">
         <div className="pb-[9.5px]">{productData.category.name}</div>
         <div className="flex items-center justify-between pb-[49px]">
           <div className="flex items-center gap-[15px] justify-between">
-            <div className="text-2xl">{productData.name}</div>
-            <Image src={HeartInactive} alt="좋아요" width={24} height={24} />
+            <div className="text-2xl font-semibold">{productData.name}</div>
+            <Image src={HeartInactive} alt="좋아요" width={28} height={28} />
           </div>
-          <div className="flex items-center justify-between gap-[10px]">
-            <Image src={KakaoLink} alt="카카오링크" width={24} height={24} />
-            <Image src={LinkShare} alt="링크공유" width={24} height={24} />
+          <div className="flex items-center justify-between gap-[10px] ">
+            <Image src={KakaoLink} alt="카카오링크" width={28} height={28} />
+            <Image src={LinkShare} alt="링크공유" width={28} height={28} />
           </div>
         </div>
-        <div className="text-[16px] pb-[60px]">{productData.description}</div>
+        <div className="text-[16px] pb-[60px] font-normal">
+          {productData.description}
+        </div>
         <div className="flex items-center justify-between">
-          <div>버튼1</div>
-          <div>버튼2</div>
+          <div>버튼1 머지 하고 이거 넣어야 해요</div>
+          <div>버튼2 머지 하고 이거 넣어야 해요</div>
         </div>
       </div>
     </div>
