91e31b7838451ab3ff81f66dd2c031a669570234
ramong26 <rlatndus21049@gmail.com>
2025-05-27 23:40:20 +0900

    refactor: 리액트쿼리 개발자 도구 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`${roboto.variable} antialiased`}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
--- src/app/react-query-provider.tsx
+++ src/app/react-query-provider.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
@@ -11,6 +11,9 @@ export default function ReactQueryProvider({
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
