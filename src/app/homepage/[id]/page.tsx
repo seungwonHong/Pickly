028ade9afb5a9308a3b897d14ff15369f644f8d9
seungwonHong <nonamed814@gmail.com>
2025-05-28 23:03:20 +0900

    feat: 리뷰어 랭킹 컴포넌트 구현

--- src/app/homepage/[id]/page.tsx
+++ src/app/homepage/[id]/page.tsx
@@ -2,7 +2,11 @@ import Header from "@/components/shared/Header";
 import Category from "@/features/home/components/Category";
 import ReviewerRanking from "@/features/home/components/ReviewerRanking";
 
-export default function HomePage({ params }: { params: { id: string } }) {
+// next 15 부터 동적 라우팅은 비동기로 처리된다
+// 따라서 params도 promise 형태로 감싸야 한다
+export default async function CategoryPage({ params }: { params: Promise<{ id: string }> }) {
+const {id: categoryId} = await params;
+
   return (
     <div>
       <header>
@@ -11,7 +15,7 @@ export default function HomePage({ params }: { params: { id: string } }) {
 
       <div className="flex flex-row lg:justify-between lg:px-[180px] md:px-0 px-[20px]">
         <div className="md:flex hidden">
-          <Category categoryId={params.id} />
+          <Category categoryId={categoryId} />
         </div>
 
         <ReviewerRanking />
---
+++ src/components/shared/FloatingButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const FloatingButton = (props: Props) => {
+  return <div>FloatingButton</div>;
+};
+
+export default FloatingButton;
