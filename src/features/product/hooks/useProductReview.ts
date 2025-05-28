f33b292043e68ca80590892761bc1de1a5a44668
ramong26 <rlatndus21049@gmail.com>
2025-05-28 11:32:02 +0900

    오타 수정

--- src/features/product/hooks/useProductReview.ts
+++ src/features/product/hooks/useProductReview.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'next/navigation'
+
 import { productService, userService } from '../api'
 
 export default function useProductIdReview() {
