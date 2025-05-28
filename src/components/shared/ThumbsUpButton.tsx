d6a6c18fae05c54c49af18fb97f64357331fd052
ramong26 <rlatndus21049@gmail.com>
2025-05-28 22:53:24 +0900

    style:반응형 추가

--- src/components/shared/ThumbsUpButton.tsx
+++ src/components/shared/ThumbsUpButton.tsx
@@ -11,17 +11,16 @@ export default function ThumbsUpButton() {
   return (
     <button
       onClick={toggleLike}
-      className={`flex gap-[5px] items-center justify-center px-[12px] py-[6px] border rounded-2xl cursor-pointer transition-all
+      className={`flex gap-[5px] items-center justify-center lg:px-[12px] px-[10px] py-[6px]  border rounded-2xl cursor-pointer transition-all
         ${isLikedState ? "border-[#5363FF] bg-[#1F2235]" : "border-[#353542]"}
         hover:scale-105 active:scale-95`}
     >
       <Image
         src={isLikedState ? ThumbsActive : ThumbsInactive}
         alt="Thumbs Up Icon"
-        width={18}
-        height={18}
+        className="w-[16px] h-[16px] lg:w-[18px] lg:h-[18px] "
       />
-      <div className="text-[14px] text-[#5363FF] font-normal">
+      <div className="lg:text-[14px] text-[12px] text-[#5363FF] font-normal">
         {islikeCount}
       </div>
     </button>
