327d017dcb992f8f86dfe2605039f3bb7d37c515
ramong26 <rlatndus21049@gmail.com>
2025-05-28 16:46:07 +0900

    style:버튼추가

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,10 @@
-import ProductIdReview from '@/features/productId/components/ProductIdReview';
+import Header from "@/components/shared/Header";
+import ProductIdReview from "@/features/productId/components/ProductIdReview";
 
 export default function ProductIdPage() {
   return (
     <div>
+      <Header />
       <div className="w-[940px] h-[1687px] mx-auto my-[60px]">
         <ProductIdReview />
       </div>
--- src/features/productId/components/ProductIdReview.tsx
+++ src/features/productId/components/ProductIdReview.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 
 import useGetUser from "../hooks/useGetUser";
 import useGetProductId from "../hooks/useGetProductId";
+import BaseButton from "@/components/shared/BaseButton";
+import TypeButton from "@/components/shared/TypeButton";
 
 import HeartInactive from "../../../../public/svg/heart-inactive.svg";
 import KakaoLink from "../../../../public/image/kakao-link.png";
@@ -46,14 +48,40 @@ export default function ProductIdReview() {
         </div>
         {user?.id === product.userId ? (
           <div className="flex items-center justify-between">
-            <div>버튼1 머지 하고 이거 넣어야 해요</div>
-            <div>버튼2 머지 하고 이거 넣어야 해요</div>
+            {/* 리뷰작성하기 클릭시 로그인 여부에 따라 모달 */}
+            <BaseButton
+              disabled={false}
+              className="px-[123.5px] py-[22px] font-semibold text-[18px] "
+            >
+              리뷰 작성하기
+            </BaseButton>
+            <TypeButton
+              type="secondary"
+              className="px-[58.5px] py-[22px] font-semibold text-[18px]"
+            >
+              비교하기
+            </TypeButton>
           </div>
         ) : (
           <div className="flex items-center justify-between">
-            <div>버튼1 머지 하고 </div>
-            <div>버튼2 머지 하고 </div>
-            <div>버튼3 머지 하고 </div>
+            <BaseButton
+              disabled={false}
+              className="px-[43.5px] py-[22px] font-semibold text-[18px] "
+            >
+              리뷰 작성하기
+            </BaseButton>
+            <TypeButton
+              type="secondary"
+              className="px-[43.5px] py-[22px] font-semibold text-[18px]"
+            >
+              비교하기
+            </TypeButton>
+            <TypeButton
+              type="tertiary"
+              className="px-[43.5px] py-[22px] font-semibold text-[18px]"
+            >
+              편집하기
+            </TypeButton>
           </div>
         )}
       </div>
