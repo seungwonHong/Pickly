1dbb7c9273438b017f32ef72a07f1b6f04991da3
ramong26 <rlatndus21049@gmail.com>
2025-05-28 17:48:04 +0900

    feat: 중간컴포넌트 완료

--- src/features/productId/components/ProductIdStats.tsx
+++ src/features/productId/components/ProductIdStats.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import React, { useEffect } from "react";
+import React from "react";
 import { useParams } from "next/navigation";
 
-import { productService } from "../api";
+import useGetProductId from "../hooks/useGetProductId";
 import ProductIdStatsBone from "./ProductIdStatsBone";
 
 import Heart from "../../../../public/icons/Heart.png";
@@ -15,27 +14,8 @@ export default function ProductIdStats() {
   const { id } = useParams();
   console.log("id:", id);
 
-  const {
-    data: score,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["productIdStats", id],
-    queryFn: () =>
-      productService.getProductsId(Number(id)).then((res) => res.data),
-    enabled: !!id,
-  });
-  console.log(
-    "score:",
-    score,
-    "isLoading:",
-    isLoading,
-    "isError:",
-    isError,
-    "error:",
-    error
-  );
+  const { product } = useGetProductId();
+  console.log("product:", product);
   return (
     <div className="w-[940px] h-[244px] text-amber-50 flex flex-col gap-[29px]">
       <div className="text-[20px] font-normal">상품통계</div>
@@ -43,67 +23,27 @@ export default function ProductIdStats() {
         <ProductIdStatsBone
           title="별점 평균"
           icon={Star}
-          score={score?.averageRating}
+          score={product?.rating}
+          diffValue={product.categoryMetric?.rating - product?.rating}
+          unit="점"
         />
         <ProductIdStatsBone
           title="하트 수"
           icon={Heart}
-          score={score?.heartCount}
+          score={product?.favoriteCount}
+          diffValue={
+            product.categoryMetric?.favoriteCount - product?.favoriteCount
+          }
+          unit="개"
         />
-        <ProductIdStatsBone title="댓글 수" icon={Talk} />
+        <ProductIdStatsBone
+          title="댓글 수"
+          icon={Talk}
+          score={product?.reviewCount}
+          diffValue={product.categoryMetric?.reviewCount - product?.reviewCount}
+          unit="개"
+        />
       </div>
     </div>
   );
 }
-
-// import { useQuery } from '@tanstack/react-query'
-// import { useParams } from 'next/navigation'
-
-// import { productService, userService } from '../api'
-
-// export default function useProductIdReview() {
-//   const { id } = useParams()
-
-//   // 로그인 되면 목데이터 뺄 예정
-//   const mockUser = {
-//     id: 793,
-//     nickname: '라몽이',
-//     description: '',
-//     image: null,
-//     createdAt: '2025-05-27T14:03:49.395Z',
-//     updatedAt: '2025-05-27T14:03:49.395Z',
-//     teamId: '14-6',
-//     isFollowing: false,
-//     followersCount: 0,
-//     followeesCount: 0,
-//     reviewCount: 0,
-//     averageRating: 0,
-//     mostFavoriteCategory: null,
-//   }
-
-//   const { data: user } = useQuery({
-//     queryKey: ['user'],
-//     queryFn: () => Promise.resolve(mockUser),
-//     // queryFn: () => userService.getUser().then((res) => res.data),  -> 로그인 완료되면 다시 해야합니다.
-//   })
-
-//   const {
-//     data: product,
-//     isLoading,
-//     isError,
-//     error,
-//   } = useQuery({
-//     queryKey: ['product', id],
-//     queryFn: () =>
-//       productService.getProductsId(Number(id)).then((res) => res.data),
-//     enabled: !!id,
-//   })
-
-//   return {
-//     user,
-//     product,
-//     isLoading,
-//     isError,
-//     error,
-//   }
-// }
--- src/features/productId/components/ProductIdStatsBone.tsx
+++ src/features/productId/components/ProductIdStatsBone.tsx
@@ -1,36 +1,50 @@
-import Image from 'next/image'
-import type { StaticImageData } from 'next/image'
-
-import Heart from '../../../../public/icons/Heart.png'
-import Star from '../../../../public/icons/Star.png'
-import Talk from '../../../../public/icons/Talk.png'
+import Image from "next/image";
+import type { StaticImageData } from "next/image";
 
 interface ProductIdStatsBoneProps {
-  title: string
-  icon: StaticImageData
-  score: number
+  title: string;
+  icon: StaticImageData;
+  score: number;
+  diffValue?: number;
+  unit: string;
 }
 
 export default function ProductIdStatsBone({
   title,
   icon,
   score,
+  diffValue,
+  unit,
 }: ProductIdStatsBoneProps) {
   return (
     <div className="w-[300px] h-[190px] flex justify-center items-center mx-auto border-[1px] rounded-[12px] border-[#353542] bg-[#252530]">
-      <div className="w-[152px] h-[130px]  flex justify-between items-center flex-col">
+      <div className="w-[152px] h-[130px] flex justify-between items-center flex-col">
         <div className="text-[18px] font-medium">{title}</div>
-        <div className="flex gap-[5px]">
+        <div className="flex gap-[5px] items-center justify-between">
           <Image
             src={icon}
             alt="별 / 하트 / 말풍선 아이콘"
-            width={24}
-            height={24}
+            className="w-[24px] h-[24px]"
           />
-          <div>{score}</div>
+          <div className="text-[24px] font-[300] text-[#9FA6B2]">{score}</div>
         </div>
-        <div className="text-[14px] font-[300]">같은 카테고리 </div>
+        <div className="text-[13px] font-[300] text-[#9FA6B2] text-center">
+          {typeof diffValue === "number" && (
+            <>
+              같은 카테고리의 제품들보다
+              <span className="text-[#f1f1f5] font-medium">
+                {Math.abs(diffValue)}
+                {unit}
+              </span>
+              {diffValue > 0
+                ? "더 적어요!"
+                : diffValue < 0
+                ? "더 많아요!"
+                : "같은 수준이에요!"}
+            </>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
