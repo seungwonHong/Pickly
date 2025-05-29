fa9cb7bf191cf96e632842301366d41a60a436f6
ramong26 <rlatndus21049@gmail.com>
2025-05-29 13:13:11 +0900

    feat:드롭다운 기능 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,20 +1,28 @@
-import BaseButton from '@/components/shared/BaseButton'
-import TypeButton from '@/components/shared/TypeButton'
+"use client";
+
+import SortDropDown from "@/components/shared/SortDropDown";
+import { useState } from "react";
+type SelectOption = {
+  value: string;
+  name: string;
+};
 
 export default function TestPage() {
-  const isState = true
+  // Available values : recent, ratingDesc, ratingAsc, likeCount
+  const selectList: SelectOption[] = [
+    { value: "recent", name: "최신순" },
+    { value: "ratingDesc", name: "별점 높은순" },
+    { value: "ratingAsc", name: "별점 낮은순" },
+    { value: "likeCount", name: "좋아요순" },
+  ];
+  const [selectedOption, setSelectedOption] = useState("recent");
   return (
     <div className="bg-[#1c1c22] w-full h-full flex flex-col items-center justify-center gap-4 ">
-      <BaseButton disabled={isState} className="py-[22px] px-[288.5px]">
-        가입하기
-      </BaseButton>
-      <BaseButton disabled={!isState} className="py-[22px] px-[288.5px]">
-        가입하기
-      </BaseButton>
-      <TypeButton className="py-[12px] px-[188.5px]">가입하기</TypeButton>
-      <TypeButton type="tertiary" className="py-[12px] px-[188.5px]">
-        가입하기
-      </TypeButton>
+      <SortDropDown
+        selectList={selectList}
+        selected={selectedOption}
+        onChange={(value) => setSelectedOption(value)}
+      />
     </div>
-  )
+  );
 }
---
+++ src/components/shared/SortDropDown.tsx
@@ -0,0 +1,84 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import SortArrow from "../../../public/icons/sort-arrow.png";
+interface OptionType {
+  name: string;
+  value: string;
+}
+
+interface DropDownProps {
+  selectList: OptionType[];
+  selected: string;
+  onChange: (value: string) => void;
+}
+
+const SortDropDown = ({ selectList, selected, onChange }: DropDownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (value: string) => {
+    onChange(value);
+    setIsOpen(false);
+  };
+
+  // 바깥 클릭시 드롭다운 닫기
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div className="relative" ref={selectRef}>
+      <div
+        className="w-[200px] h-[24px] px-[20px]  cursor-pointer flex items-center justify-between text-[#F1F1F5]"
+        onClick={toggleDropdown}
+      >
+        {selectList.find((item) => item.value === selected)?.name}
+        <Image
+          src={SortArrow}
+          alt="Arrow"
+          className={`cursor-pointer transition-transform duration-200 ease-in-out ${
+            isOpen ? "rotate-180" : ""
+          }`}
+        />
+      </div>
+      {isOpen && (
+        <ul className="absolute top-[34px]  w-full border z-10 flex flex-col items-center justify-around list-none p-[10px] gap-[5px] border-[#626282] rounded-[8px] bg-[#252530] ">
+          {selectList.map((item) => {
+            const isSelected = item.value === selected;
+            return (
+              <li
+                key={item.value}
+                className={`w-[180px] px-[20px] py-[5px] cursor-pointer flex items-center rounded-[8px] transition-colors duration-100
+            ${
+              isSelected
+                ? "bg-[#3A3A48] text-[#F1F1F5] text-[16px]"
+                : "text-[#A0A0B0] text-[16px] hover:bg-[#3A3A48] hover:text-white"
+            }`}
+                onClick={() => handleSelect(item.value)}
+              >
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SortDropDown;
