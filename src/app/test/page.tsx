8911202614038fe92cd75dc218631854683f4ef9
ramong26 <rlatndus21049@gmail.com>
2025-06-05 09:40:28 +0900

    달리진거 거 ㅇ

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,30 +1,67 @@
-
 "use client";
+import { useSearchParams, useRouter } from "next/navigation";
 
-import SortDropDown from "@/components/shared/SortDropDown";
-import { useState } from "react";
-type SelectOption = {
-  value: string;
-  name: string;
-};
+import BaseButton from "@/components/shared/BaseButton";
+import TypeButton from "@/components/shared/TypeButton";
+import ProductCompareChangeModal from "@/features/productId/components/modal/ProductCompareModal/ProductCompareChangeModal";
 
-export default function TestPage() {
-  // Available values : recent, ratingDesc, ratingAsc, likeCount
-  const selectList: SelectOption[] = [
-    { value: "recent", name: "최신순" },
-    { value: "ratingDesc", name: "별점 높은순" },
-    { value: "ratingAsc", name: "별점 낮은순" },
-    { value: "likeCount", name: "좋아요순" },
-  ];
-  const [selectedOption, setSelectedOption] = useState("recent");
+export default function ProductIdDetailButton() {
+  // 모달 열기 및 닫기 로직 (이렇게 할 수 있다니...ㄷㄷ...)
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  // 비교하기 상품이 교체되었어요 모달
+  const isReviewModalOpen = searchParams.get("modal") === "compareChange";
+  const openReviewModal = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("modal", "compareChange");
+    router.replace(`?${params.toString()}`, { scroll: false });
+  };
+  const closeReviewModal = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("modal");
+    router.replace(`?${params.toString()}`, { scroll: false });
+  };
+
+  // 비교하기 모달
+  // const isCompareModalOpen = searchParams.get("modal") === "compare";
+  // const openCompareModal = () => {
+  //   const params = new URLSearchParams(searchParams.toString());
+  //   params.set("modal", "compare");
+  //   router.replace(`?${params.toString()}`, { scroll: false });
+  // };
+  // const closeCompareModal = () => {
+  //   const params = new URLSearchParams(searchParams.toString());
+  //   params.delete("modal");
+  //   router.replace(`?${params.toString()}`, { scroll: false });
+  // };
+
+  // 쿠키 이용하면 로딩중 버튼 갯수 빠르게 로딩 가능ㄴㄴ
   return (
-    <div className="bg-[#1c1c22] w-full h-full flex flex-col items-center justify-center gap-4 ">
-      <SortDropDown
-        selectList={selectList}
-        selected={selectedOption}
-        onChange={(value) => setSelectedOption(value)}
-      />
-
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <BaseButton
+          disabled={false}
+          className="px-[123.5px] py-[22px] font-semibold text-[18px] "
+          onClick={openReviewModal}
+        >
+          비교상품이교체되었어요 모달
+        </BaseButton>
+        <TypeButton
+          type="secondary"
+          className="px-[58.5px] py-[22px] font-semibold text-[18px]"
+          // onClick={openCompareModal}
+        >
+          비교하기
+        </TypeButton>
+      </div>
+      {/* 리뷰 작성 모달 컴포넌트 */}
+      {isReviewModalOpen && (
+        <ProductCompareChangeModal
+          open={isReviewModalOpen}
+          setOpen={closeReviewModal}
+        />
+      )}
+    </>
   );
 }
