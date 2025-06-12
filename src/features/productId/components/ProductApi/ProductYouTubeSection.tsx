ee5b573b3a5e0f2e5de74137d9c5232658fbf11c
ramong26 <rlatndus21049@gmail.com>
2025-06-12 20:09:59 +0900

    style/유튜브뮤직 스켈레톤

--- src/features/productId/components/ProductApi/ProductYouTubeSection.tsx
+++ src/features/productId/components/ProductApi/ProductYouTubeSection.tsx
@@ -3,15 +3,12 @@
 import { useYouTubeQuery } from "@/features/productId/hooks/useGetMusicvideo";
 
 export default function ProductYouTubeSection({ query }: { query: string }) {
-  const { data: videos, error } = useYouTubeQuery(query);
-
-  if (error) return <div>오류 발생: {(error as Error).message}</div>;
+  const { data: videos } = useYouTubeQuery(query);
 
   if (!videos || videos.length === 0)
     return (
       <div className="animate-pulse">
         <div className="w-[620px] h-[350px] bg-gray-300 rounded-xl mb-4" />
-        <div className="w-1/2 h-6 bg-gray-200 rounded mt-2" />
       </div>
     );
   return (
