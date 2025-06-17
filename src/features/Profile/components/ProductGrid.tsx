7c62ee33e76d0e5a8e4f6f9003744a0fb1f847f0
sssson0 <s20023094@gmail.com>
2025-06-17 14:09:18 +0900

    .

--- src/features/Profile/components/ProductGrid.tsx
+++ src/features/Profile/components/ProductGrid.tsx
@@ -1,40 +1,49 @@
 import { useUserProducts } from "../hook/useUserProducts";
 import { Product, ProductTabType } from "../types/user";
-import UserProductCard from "./UserProductCard";
+import { ProductList } from "@/features/home/types/productType";
+import ProductCard from "@/components/shared/ProductCard";
 
 interface Props {
   userId: number;
   type: ProductTabType;
   initialProducts?: Product[];
 }
+
 export default function ProductGrid({ userId, type, initialProducts }: Props) {
   const { products, ref } = useUserProducts({
     userId,
     type,
     initialData: initialProducts,
   });
+
   return (
-    <>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <UserProductCard
-              key={product.id}
-              id={product.id}
-              name={product.name}
-              image={product.image}
-              rating={product.rating}
-              favoriteCount={product.favoriteCount}
-              reviewCount={product.reviewCount}
-            />
-          ))
-        ) : (
-          <div className="col-span-2 md:col-span-3 text-center text-gray-400 pt-[100px]">
-            상품이 없습니다.
-          </div>
-        )}
-        <div ref={ref} />
-      </div>
-    </>
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.length > 0 ? (
+        products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={
+              {
+                id: product.id,
+                name: product.name,
+                image: product.image,
+                categoryId: product.categoryId ?? 0,
+                reviewCount: product.reviewCount,
+                favoriteCount: product.favoriteCount,
+                rating: product.rating,
+                writerId: 0,
+                createdAt: "",
+                updatedAt: "",
+              } as ProductList
+            }
+          />
+        ))
+      ) : (
+        <div className="col-span-2 md:col-span-3 text-center text-gray-400 pt-[100px]">
+          상품이 없습니다.
+        </div>
+      )}
+      <div ref={ref} />
+    </div>
   );
 }
--- src/features/Profile/components/UserProductCard.tsx
+++ src/features/Profile/components/UserProductCard.tsx
@@ -1,47 +1,47 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-interface ProductCardProps {
-  id: number;
-  name: string;
-  image: string;
-  reviewCount: number;
-  favoriteCount: number;
-  rating: number;
-}
+// interface ProductCardProps {
+//   id: number;
+//   name: string;
+//   image: string;
+//   reviewCount: number;
+//   favoriteCount: number;
+//   rating: number;
+// }
 
-export default function ProductCard({
-  id,
-  name,
-  image,
-  reviewCount,
-  favoriteCount,
-  rating,
-}: ProductCardProps) {
-  return (
-    <Link href={`/product/${id}`}>
-      <div className="flex flex-col p-[15px] justify-center relative  border border-[#353542] bg-[#252530]  w-full rounded-[8px] z-[-1]">
-        <div className="flex justify-center relative items-center overflow-hidden w-full  h-[98px] mb-[10px] md:mb-[20px] md:h-[160px] lg:h=[200px] lg:mb-[25px] rounded-[8px]">
-          <img
-            src={image}
-            alt="productImage"
-            className="object-cover w-full h-full"
-          />
-        </div>
-        <p className="text-white font-semibold text-[14px] mb-[5px] md:mb-[10px] md:text-[16px] lg:text-[18px] ">
-          {name}
-        </p>
-        <div className="flex flex-col md:flex-row md:justify-between">
-          <div className="flex gap-[10px] text-[#6E6E82] font-normal text-[12px] md:text-[14px] md:gap-[5px] lg:text-[16px]">
-            <p>리뷰 {reviewCount}</p>
-            <p>찜 {favoriteCount}</p>
-          </div>
-          <div className="flex items-center gap-[3px]">
-            <p className="text-[#9FA6B2] text-[12px] font-normal md:text-[14px] lg:text-[16px]">
-              ⭐️ {Math.round(rating * 10) / 10}
-            </p>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
-}
+// export default function ProductCard({
+//   id,
+//   name,
+//   image,
+//   reviewCount,
+//   favoriteCount,
+//   rating,
+// }: ProductCardProps) {
+//   return (
+//     <Link href={`/product/${id}`}>
+//       <div className="flex flex-col p-[15px] justify-center relative  border border-[#353542] bg-[#252530]  w-full rounded-[8px] z-[-1]">
+//         <div className="flex justify-center relative items-center overflow-hidden w-full  h-[98px] mb-[10px] md:mb-[20px] md:h-[160px] lg:h=[200px] lg:mb-[25px] rounded-[8px]">
+//           <img
+//             src={image}
+//             alt="productImage"
+//             className="object-cover w-full h-full"
+//           />
+//         </div>
+//         <p className="text-white font-semibold text-[14px] mb-[5px] md:mb-[10px] md:text-[16px] lg:text-[18px] ">
+//           {name}
+//         </p>
+//         <div className="flex flex-col md:flex-row md:justify-between">
+//           <div className="flex gap-[10px] text-[#6E6E82] font-normal text-[12px] md:text-[14px] md:gap-[5px] lg:text-[16px]">
+//             <p>리뷰 {reviewCount}</p>
+//             <p>찜 {favoriteCount}</p>
+//           </div>
+//           <div className="flex items-center gap-[3px]">
+//             <p className="text-[#9FA6B2] text-[12px] font-normal md:text-[14px] lg:text-[16px]">
+//               ⭐️ {Math.round(rating * 10) / 10}
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// }
