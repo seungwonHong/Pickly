48cdec638b6fb4871afde552118e0f0d7bf1c4ed
seungwonHong <nonamed814@gmail.com>
2025-06-18 20:56:28 +0900

    fix:16

--- src/features/compare/components/CompareProductInputSecond.tsx
+++ src/features/compare/components/CompareProductInputSecond.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { useRef, useState } from "react";
 import CompareDropdown from "./CompareDropdown";
 import useProductSearch from "../hooks/useProductSearch";
