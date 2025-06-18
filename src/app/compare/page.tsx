0efd4eb14456a32a6d54eae118e906f9166b7985
seungwonHong <nonamed814@gmail.com>
2025-06-18 20:23:40 +0900

    fix:10

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,15 +1,17 @@
-"use client";
-
 import CompareProductForm from "@/features/compare/components/CompareProductForm";
 import Header from "@/components/shared/Header";
+import { Suspense } from "react";
+import LoadingClient from "./loadingClient";
 
 export default function Compare() {
   return (
     <div>
       <Header />
       <div className="flex flex-col mt-[40px]">
-        <CompareProductForm />
+        <Suspense fallback={<LoadingClient />}>
+          <CompareProductForm />
+        </Suspense>
       </div>
     </div>
-  )
+  );
 }
